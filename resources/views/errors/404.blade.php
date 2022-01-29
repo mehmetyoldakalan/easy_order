b8de0385e604248d6a09bb54ab3ccd4f9b7411ab
mehmetyoldakalann <mnyoldakalan@gmail.com>
2022-01-29 13:58:08 +0300

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>404 Not Found :(</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="assets/css/vendor.min.css" rel="stylesheet">
+    <link href="assets/css/app.min.css" rel="stylesheet">
+
+</head>
+
+<body class='pace-top'>
+
+    <div id="app" class="app app-full-height app-without-header">
+
+        <div class="error-page">
+
+            <div class="error-page-content">
+                <div class="card mb-5 mx-auto" style="max-width: 320px;">
+                    <div class="card-body">
+                        <div class="card">
+                            <div class="error-code">404</div>
+                            <div class="card-arrow">
+                                <div class="card-arrow-top-left"></div>
+                                <div class="card-arrow-top-right"></div>
+                                <div class="card-arrow-bottom-left"></div>
+                                <div class="card-arrow-bottom-right"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-arrow">
+                        <div class="card-arrow-top-left"></div>
+                        <div class="card-arrow-top-right"></div>
+                        <div class="card-arrow-bottom-left"></div>
+                        <div class="card-arrow-bottom-right"></div>
+                    </div>
+                </div>
+                <h1>Oops!</h1>
+                <h3>Görünüşe göre aradığınız sayfayı bulamıyoruz</h3>
+                <hr>
+                <p class="mb-1">
+                </p>
+
+                <a href="javascript:window.history.back();" class="btn btn-outline-theme px-3 rounded-pill"><i
+                        class="fa fa-arrow-left me-1 ms-n1"></i> Geri Dönelim</a>
+            </div>
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+</body>
+
+</html>
